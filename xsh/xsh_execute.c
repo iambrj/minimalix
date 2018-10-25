b8257e185d9dfdec2f4b035e75d6c0dee08a92ef
brj <bharathiramanajoshi1@gmail.com>
2018-10-26 02:08:56 +0530

    added builtins

--- xsh/xsh_execute.c
+++ xsh/xsh_execute.c
@@ -1,10 +1,21 @@
-include "decl.h"
+#include "decl.h"
 #include "builtins.h"
 int xsh_execute(char **args) {
-  int i;
+	int i;
 
-  if (args[0] == NULL) {
-    return 1;
-  }
-  return xsh_launch(args);
+	if (args[0] == NULL) {
+		return 1;
+	}
+	//TODO: add array of function pointers to manage builtins
+	if(strcmp(args[0], "history") == 0) {
+		xsh_history();
+		return 1;
+	} else if(strcmp(args[0], "cd") == 0) {
+		xsh_cd(args);
+		return 1;
+	} else if(strcmp(args[0], "echo") == 0) {
+		xsh_echo(args);
+		return 1;
+	}
+	return xsh_launch(args);
 }
